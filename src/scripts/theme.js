092d4f0f57d684bbb2b34a6bef39e6c84472e881
Victor Carvalho de Jesus <cictorvarvalho@gmail.com>
2023-10-31 14:06:53 -0300

    att

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,13 @@
 /* Desenvolva sua lógica aqui ... */
+
+const button = document.querySelector('.header__btn');
+const body = document.body
+let darkMode = false;
+
+function themeChange () {
+    darkMode = !darkMode;
+    button.classList.toggle('header__btn--dark-mode');
+    body.classList.toggle('dark-mode:root')
+}
+
+button.addEventListener('click',themeChange);
